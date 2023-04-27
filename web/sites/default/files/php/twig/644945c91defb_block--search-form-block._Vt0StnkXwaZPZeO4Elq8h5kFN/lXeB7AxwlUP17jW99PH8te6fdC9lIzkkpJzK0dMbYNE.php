77926ba15b79709c9fafc53a80f5a4c260bfc795
saikumar1973 <saikumarsabhavath1973@gmail.com>
2023-04-27 13:46:39 +0530

    project moved to git

---
+++ web/sites/default/files/php/twig/644945c91defb_block--search-form-block._Vt0StnkXwaZPZeO4Elq8h5kFN/lXeB7AxwlUP17jW99PH8te6fdC9lIzkkpJzK0dMbYNE.php
@@ -0,0 +1,169 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/olivero/templates/block/block--search-form-block.html.twig */
+class __TwigTemplate_b317ce2d2d7f5495356a63e38c394e69 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 18
+        $context["classes"] = [0 => "block", 1 => "block-search"];
+        // line 23
+        echo "<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 23), 23, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 24
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 24, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 25
+        if (($context["label"] ?? null)) {
+            // line 26
+            echo "    <h2";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_attributes"] ?? null), 26, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 26, $this->source), "html", null, true);
+            echo "</h2>
+  ";
+        }
+        // line 28
+        echo "  ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 28, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 29
+        $this->displayBlock('content', $context, $blocks);
+        // line 34
+        echo "</div>
+";
+    }
+
+    // line 29
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 30
+        echo "    <div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [0 => "content", 1 => "container-inline"], "method", false, false, true, 30), 30, $this->source), "html", null, true);
+        echo ">
+      ";
+        // line 31
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 31, $this->source), "html", null, true);
+        echo "
+    </div>
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/olivero/templates/block/block--search-form-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  82 => 31,  77 => 30,  73 => 29,  68 => 34,  66 => 29,  61 => 28,  53 => 26,  51 => 25,  47 => 24,  42 => 23,  40 => 18,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Olivero's theme implementation for a search form block.
+ *
+ * Available variables:
+ * - content: The content of this block.
+ * - content_attributes: A list of HTML attributes applied to the main content
+ *   tag that appears in the template.
+ *
+ * @see template_preprocess_block()
+ * @see search_preprocess_block()
+ *
+ * @ingroup themeable
+ */
+#}
+{%
+  set classes = [
+    'block',
+    'block-search',
+  ]
+%}
+<div{{ attributes.addClass(classes) }}>
+  {{ title_prefix }}
+  {% if label %}
+    <h2{{ title_attributes }}>{{ label }}</h2>
+  {% endif %}
+  {{ title_suffix }}
+  {% block content %}
+    <div{{ content_attributes.addClass('content', 'container-inline') }}>
+      {{ content }}
+    </div>
+  {% endblock %}
+</div>
+", "core/themes/olivero/templates/block/block--search-form-block.html.twig", "C:\\xampp\\htdocs\\project\\web\\core\\themes\\olivero\\templates\\block\\block--search-form-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 18, "if" => 25, "block" => 29);
+        static $filters = array("escape" => 23);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'block'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
